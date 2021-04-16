c9ed9904233a38382ab42ab5b94b09ccb4229f48
Szymon Niemiec <matem@tyka.pl>
2021-04-16 17:32:48 +0200

    Cards shown

--- decks.js
+++ decks.js
@@ -1,9 +1,9 @@
 const taskDecks = [{
-    items: 12,
+    items: 10,
     prizes: [1000, 1500],
     punishmentFactor: [1, 2]
 }, {
-    items: 12,
+    items: 11,
     prizes: [5000, 7000],
     punishmentFactor: [2, 3]
 }, {
@@ -18,3 +18,10 @@ const punishmentDecks = [
     [50, 55, 60, 70, 75, 80, 90, 99, 100, "10%", 121, 199, 1000, "20%", 3000],
     ["15%", 5000, "30%", 9000, "90%", "99%"]
 ];
+
+function shuffleCards(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
--- script.js
+++ script.js
@@ -102,14 +102,13 @@ const makeCovers = (grid) => {
     return covers;
 };
 
-const makeGoals = (grid) => {
+const makeGoals = () => {
     var goals = new Array();
     var goalsCreated = 0;
     for(var i = 1; i <= config.items; i++) {
         var element = document.createElement("div");
         element.id = "goal" + goalsCreated++
         document.body.appendChild(element);
-        goals[i] = element;
         
         var y = i > 6 ? 1 : 0;
         var x = i > 6 ? i - 13 : i - 7;
@@ -117,10 +116,164 @@ const makeGoals = (grid) => {
         $("#"+element.id).css("left", "calc(100% - " + (-11 * x) +"vmin)");
         $("#"+element.id).css("top", (y + 0.25) * 11 + "vmin");
         $("#"+element.id).css("z-index", 3);
+        $("#"+element.id).css("padding", "5px");
+        $("#"+element.id).css("background-color", "rgba(0,15,0,0.95)");
         $("#"+element.id).css("background-image", "url('images/"+i+".svg')");
         goals[i] = element;
     }
-    return covers;
+    return goals;
+};
+
+const getPunushmentText = (punishment, element) => {
+    if(Array.isArray(punishment))
+        return punishment.map(p => getPunushmentText(p, element)).join(" + ");
+    if(typeof punishment == 'number')
+        element.dataset.number=punishment;
+    else
+        element.dataset.percentage=punishment.replace("%", '');
+    return "" + punishment;
+}
+
+const makeCards = (goals) => {
+    var cards = new Array();
+    var numberOfDecks = 0;
+    for(const taskDeck of taskDecks) {
+        numberOfDecks++;
+        var cardsCreated = 0;
+        cards[numberOfDecks] = new Array();
+        for(var subset = 0; subset < taskDeck.prizes.length; subset++)
+        for(var i = 1; i <= taskDeck.items; i++) {
+            var front = document.createElement("div");
+            front.id = "card.front." + cardsCreated;
+            front.style.backgroundImage = "url('images/front.svg')";
+            front.style.width = "10.5vmin";
+            front.style.height = "15.75vmin";
+            front.style.display = "flex";
+            front.style.justifyContent = "center";
+            front.style.paddingTop = "3px";
+            var clone = goals[i].cloneNode(true);
+            clone.id = "card.front." + cardsCreated + "picture";
+            clone.style.position = "absolute";
+            clone.style.left = "0.75vmin";
+            clone.style.top = "3.625vmin";
+            clone.style.width = "9vmin";
+            clone.style.height = "9vmin";
+            clone.style.padding = "0px";
+            front.appendChild(clone);
+            var text = document.createTextNode(
+                taskDeck.prizes[subset]
+            );
+            front.appendChild(text);
+            front.classList.add("goal");
+            front.style.visibility = "hidden";
+            front.dataset.goal = i;
+            front.dataset.prize = taskDeck.prizes[subset];
+            document.body.appendChild(front);
+
+            var back = document.createElement("div");
+            back.id = "card.back." + cardsCreated;
+            back.style.backgroundImage = "url('images/back" + numberOfDecks + ".svg')";
+            back.style.width = "10.5vmin";
+            back.style.height = "15.75vmin";
+            document.body.appendChild(back);
+
+            cards[numberOfDecks][cardsCreated++] = {
+                front: front,
+                back: back
+            };
+        }
+    }
+
+    var numberOfGoalCards = numberOfDecks;
+    var numberOfPunishmentDecks = 0;
+    for(const deck of punishmentDecks) {
+        numberOfDecks++;
+        numberOfPunishmentDecks++;
+        var cardsCreated = 0;
+        cards[numberOfDecks] = new Array();
+        for(var punishment of deck) {
+            var front = document.createElement("div");
+            front.id = "card.front." + cardsCreated;
+            front.style.backgroundImage = "url('images/front.svg')";
+            front.style.width = "10.5vmin";
+            front.style.height = "15.75vmin";
+            front.classList.add("punishment");
+            front.style.display = "flex";
+            front.style.justifyContent = front.style.alignItems = "center";
+            var text = document.createTextNode(
+                getPunushmentText(punishment, front)
+            );
+            front.appendChild(text);
+            front.style.visibility = "hidden";
+            document.body.appendChild(front);
+
+            var back = document.createElement("div");
+            back.id = "card.back." + cardsCreated;
+            back.style.backgroundImage = "url('images/back-" + numberOfPunishmentDecks + ".svg')";
+            back.style.width = "10.5vmin";
+            back.style.height = "15.75vmin";
+            document.body.appendChild(back);
+
+            cards[numberOfDecks][cardsCreated++] = {
+                front: front,
+                back: back
+            };
+        }
+    }
+
+    for(var deckNumber = 1; deckNumber <= numberOfGoalCards; deckNumber++) 
+        shuffleCards(cards[deckNumber]);
+
+    var specialPunishmentDeck = cards[numberOfGoalCards + 1];
+    for(var i = 1; i <= config.items; i++) {
+        specialPunishmentDeck[i-1].front.style.left = goals[i].style.left.replace(")", " - 2.5vmin)");
+        specialPunishmentDeck[i-1].back.style.left = goals[i].style.left.replace(")", " - 2.5vmin)");
+        specialPunishmentDeck[i-1].front.style.top = 'calc(' + goals[i].style.top + " - 2.5vmin)";
+        specialPunishmentDeck[i-1].back.style.top = 'calc(' + goals[i].style.top + " - 2.5vmin)";
+        specialPunishmentDeck[i-1].front.dataset.special = true;
+        specialPunishmentDeck[i-1].back.dataset.special = true;
+    }
+
+    for(var deckNumber = numberOfGoalCards + 2; deckNumber <= numberOfDecks; deckNumber++) 
+        shuffleCards(cards[deckNumber]);
+
+    var orderedCards = {
+        goals: new Array(),
+        punishments: new Array(),
+        specials: new Array()
+    };
+
+    var index = 0;
+    for(var deckNumber = 1; deckNumber <= numberOfGoalCards; deckNumber++) 
+    for(var card of cards[deckNumber])
+        orderedCards.goals[index++] = card;
+
+    index = 0;
+    for(var card of specialPunishmentDeck)
+        orderedCards.specials[index++] = card;
+    
+    var index = 0;
+    for(var deckNumber = numberOfGoalCards + 2; deckNumber <= numberOfDecks; deckNumber++)
+    for(var card of cards[deckNumber])
+        orderedCards.punishments[index++] = card;
+
+    var index = 0;
+    for(var card of orderedCards.goals) {
+        card.front.style.zIndex = card.back.style.zIndex = orderedCards.goals.length + 3 - (index);
+        card.front.style.top = card.back.style.top = "calc(2vh + 5.5vmin + " + (index / 6) + "px)";
+        card.front.style.left = card.back.style.left = (1.5 + index / 13.1) + "vw";
+        index++;
+    }
+
+    var index = 0;
+    for(var card of orderedCards.punishments) {
+        card.front.style.zIndex = card.back.style.zIndex = orderedCards.goals.length + 3 - (index);
+        card.front.style.top = card.back.style.top = "calc(2vh + 5.5vmin + " + (index / 6) + "px)";
+        card.front.style.left = card.back.style.left = (12.5 + index / 13.1) +"vw";
+        index++;
+    }
+
+    return orderedCards;
 };
 
 
@@ -165,4 +318,21 @@ const findClickables = (position, covers) => {
             $("#" + covers[x][y].id).attr('data-y', null);
         }
     }
-}
+};
+
+const getCurrentGoal = (cards, goals) => {
+    for(var card of cards.goals){
+        if(card.front.dataset.used) continue;
+        card.front.style.visibility = "visible";
+        $(card.front).hide(11);
+        $(card.front).show(611);
+        $(card.back).hide(311);
+        card.front.dataset.used = true;
+        for(var i = 1; i<goals.length; i++) {
+            goals[i].style.border = i == card.front.dataset.goal ? "solid blue 5px" : "0px";
+            goals[i].style.padding = i == card.front.dataset.goal ? "0px" : "5px";
+            goals[i].style.borderRadius = "3px";
+        }
+        return card.front.dataset.goal;
+    }
+};
